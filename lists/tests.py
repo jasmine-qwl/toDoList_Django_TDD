ee496529e3d9af33c2229b11f2000afa6d24184e
Qianwen Li <jasmineangel0503@gmail.com>
2021-08-23 09:07:52 +0800

    Front page can now accept user input and display the first item

--- lists/tests.py
+++ lists/tests.py
@@ -6,12 +6,12 @@ from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data = {'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
