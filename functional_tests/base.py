03b71ed7c74a2f7f7612d3955b1ddd541b3f4da1
Qianwen Li <jasmineangel0503@gmail.com>
2021-09-13 08:39:52 +0800

    first-cut FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -8,6 +8,16 @@ MAX_WAIT = 10
 
 class FunctionalTest(StaticLiveServerTestCase):
 
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
