178a861cab1b1c62c9664dd1fe580aa6df244223
Qianwen Li <jasmineangel0503@gmail.com>
2021-09-20 08:29:09 +0800

    refactor test_layout_and_styling with PageList pattern

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
+from .list_page import ListPage
 
 class LayoutAndStylingTest(FunctionalTest):
 
@@ -9,7 +10,7 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
-        inputbox = self.get_item_input_box()
+        inputbox = ListPage(self).get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
@@ -18,10 +19,8 @@ class LayoutAndStylingTest(FunctionalTest):
 
         # She starts a new list and sees the input is nicely
         # centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.get_item_input_box()
+        ListPage(self).add_list_item('testing')
+        inputbox = ListPage(self).get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
